9e9d9a0aa6bdd9c7e90e7588f6141a07b347a8cb
s-aboosh <sa.aboosh.br99@gmail.com>
2024-07-30 13:20:13 -0700

    deploy the react-app

--- src/useMovie.js
+++ src/useMovie.js
@@ -16,7 +16,11 @@ export function useMovies(query) {
           setError("");
           const res = await fetch(
             `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
+            { signal: controller.signal },
+            {
+              // ...
+              referrerPolicy: "unsafe-url",
+            }
           );
           if (!res.ok)
             throw new Error("some thing went worng with fetching movies");
